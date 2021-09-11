697cdd234ab15db6f297099ff297df173fd2b0af
Ayrton Bello <59799942+aaybe@users.noreply.github.com>
2021-09-11 01:57:38 -0500

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,14 +1,11 @@
 
 SELECT SUM(new_cases) AS total_cases, SUM(cast(new_deaths AS INT)) AS total_deaths, ROUND(SUM(CAST(new_deaths AS INT))/SUM(New_Cases)*100,6) AS DeathPercentage
 FROM CovidDataProject..CovidDeaths
---Where location like '%states%'
 WHERE continent IS NOT NULL
---Group By date
 ORDER BY 1,2
 
 SELECT location, SUM(CAST(new_deaths AS INT)) AS TotalDeathCount
 From CovidDataProject..CovidDeaths
---Where location like '%states%'
 WHERE continent IS NULL 
 AND location NOT IN ('World', 'European Union', 'International')
 GROUP BY location
@@ -16,12 +13,10 @@ ORDER BY TotalDeathCount DESC
 
 SELECT location, population, MAX(total_cases) AS HighestInfectionCount,  ROUND(MAX((total_cases/population))*100,6) AS PercentPopulationInfected
 FROM CovidDataProject..CovidDeaths
---Where location like '%states%'
 GROUP BY location, population
 ORDER BY PercentPopulationInfected DESC
 
 SELECT location, population,date, MAX(total_cases) AS HighestInfectionCount,  ROUND(MAX((total_cases/population))*100,6) AS PercentPopulationInfected
 FROM CovidDataProject..CovidDeaths
---Where location like '%states%'
 GROUP BY location, population, date
 ORDER BY PercentPopulationInfected DESC
